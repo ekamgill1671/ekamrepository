af776f368db163675c4215cbe0edeb868455d4c4
Ekam Gill <ekamgill1671@gmail.com>
2026-04-17 13:56:28 -0700

    basic coding
    -james is broke

---
+++ account.java
@@ -0,0 +1,30 @@
+
+public class account {
+
+    String name;
+    String account;
+    Double money;
+    Double balance;
+
+    public account(String name, double balance) {
+
+        this.name = name;
+        this.balance = balance;
+
+    }
+
+    public account(Double balance){
+        
+        this.balance = balance;
+    }
+
+
+    public void withdraw(double money) {
+        balance = balance - money;
+
+    }
+
+    public void greet() {
+        System.out.println(name+":" + " I am going broke because I only have " +balance);
+    }
+}
