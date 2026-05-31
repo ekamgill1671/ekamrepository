ab474e877d73c58b504a3ee23a19c46e7ef5d238
Ekam Gill <ekamgill1671@gmail.com>
2026-05-31 09:27:09 -0700

    okokko

--- bankTest.java
+++ bankTest.java
@@ -14,18 +14,34 @@ public bankTest() {
 public static void main(String[] args) {
 
     account James = new account("James", 10.0);
-    account Henry = new account("Henry", 2000.0);
+    // account Henry = new account("Henry", 2000.0);
+
+    James.withdraw2(15.0);
+    System.out.println(James.balance);
+
+  
+
+    //System.out.println(account.changeBankName("Diana's bank"));
+    // account.changeBankName("Diana's bank");
+    James.greet2();
+
+// System.out.println(account.nameBank);
+
 
 
 
    //James.withdraw(5.0);
    //James.greet();
-   Henry.withdraw(230.0);
-   Henry.greet();
-   Henry.setOccupation("Software Engineer");
-   Henry.setAge(20);
+   //Henry.withdraw(230.0);
+  // Henry.greet();
+  // Henry.setOccupation("Software Engineer");
+  // Henry.setAge(20);
 
-   Henry.areyouabozo(false);
+
+
+   // Henry.areyouabozo();
+
+   
 
    //System.out.println("Henry's age is: " + Henry.age + " and his occupation is: " + Henry.occupation);
 
@@ -33,15 +49,17 @@ public static void main(String[] args) {
 
    // Henry.getInfo();
 
-   
+   //System.out.println(account.nameBank = "Ekam's Bank");
 
    
 
+   
+
 
    
 
 
-   Henry.deposit(100.0);
+   // Henry.deposit(100.0);
    
 }
 }
